3304716fc5cf91219d2635ce54dffd7a8d4ebdb3
Alamin2017 <alamincse12@gmail.com>
2025-04-13 20:56:33 +0600

    updated

--- cypress/e2e/Test_case_06.cy.js
+++ cypress/e2e/Test_case_06.cy.js
@@ -10,14 +10,11 @@ it('should login successfully with valid credentials', () => {
     cy.xpath("//u[normalize-space()='View Cart']").click();
     cy.xpath("//a[normalize-space()='Proceed To Checkout']").click();
     cy.xpath("//u[normalize-space()='Register / Login']").click();
-
     // Generate a random email
     const randomEmail = `test${Date.now()}@example.com`;
-
     cy.xpath('//*[@id="form"]/div/div/div[3]/div/form/input[2]').type('tania');
     cy.xpath('//*[@id="form"]/div/div/div[3]/div/form/input[3]').type(randomEmail);
     cy.xpath('//*[@id="form"]/div/div/div[3]/div/form/button').click();
-
     cy.xpath('//*[@id="id_gender1"]').check();
     cy.xpath('//*[@id="password"]').type('34567890');
     cy.get('#days').select('14');
@@ -33,27 +30,21 @@ it('should login successfully with valid credentials', () => {
     cy.xpath('//*[@id="zipcode"]').type('1229');
     cy.xpath('//*[@id="mobile_number"]').type('01723456721');
     cy.xpath('//*[@id="form"]/div/div/div/div[1]/form/button').click();
-
     cy.xpath("//b[normalize-space()='Account Created!']").should('have.text', 'Account Created!');
     cy.xpath('//*[@id="form"]/div/div/div/div/a').click();
-
     cy.xpath("//header[@id='header']//li[10]").should('have.text', ' Logged in as tania');
     cy.xpath("//a[normalize-space()='Cart']").click();
     cy.xpath("//a[normalize-space()='Proceed To Checkout']").click();
-
     cy.xpath("//h2[normalize-space()='Address Details']").should('have.text', 'Address Details');
     cy.xpath("//h2[normalize-space()='Review Your Order']").should('have.text', 'Review Your Order');
-
     cy.xpath("//textarea[@name='message']").type("Already order some products");
     cy.xpath("//a[normalize-space()='Place Order']").click();
-
     cy.get('input[name="name_on_card"]').type('sumon');
     cy.get('input[name="card_number"]').type('4485233521979909');
     cy.xpath("//input[@placeholder='ex. 311']").type('748');
     cy.xpath("//input[@placeholder='MM']").type('9');
     cy.xpath("//input[@placeholder='YYYY']").type('2025');
     cy.xpath("//button[@id='submit']").click();
-
     cy.wait(2000); // Cypress discourages static waits; prefer `.should()` where possible
     cy.xpath("//p[normalize-space()='Congratulations! Your order has been confirmed!']").should('contain.text', 'Congratulations! Your order has been confirmed!');
     cy.get(":nth-child(5) > a").click();
